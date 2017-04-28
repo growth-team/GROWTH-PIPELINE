e4aa27a2af4ae024370a6037114efee0265c091a
Yuuki Wada <yuukiwada@hibiscus.local>
2017-04-28 12:15:38 +0900

    modified scanSignificance.rb

--- bin/scanSignificance.rb
+++ bin/scanSignificance.rb
@@ -5,7 +5,7 @@ include Math
 include Root
 include RootApp
 
-if ARGV[4]==nil then
+if ARGV[6]==nil then
   puts "Usage: ruby scanEventSignificance.rb <fits file list> <adc channel> <bin size (sec)> <lower energy> <upper energy> <bgd mean> <bgd sigma> <output file>"
   exit 1
 end
@@ -17,7 +17,14 @@ energyLimitsLow=ARGV[3].to_f
 energyLimitsHigh=ARGV[4].to_f
 bgdMean=ARGV[5].to_f
 bgdSigma=ARGV[6].to_f
-outputFile=ARGV[7]
+
+outputAll=File.open("scanResult_all_significance.dat", "w")
+outputSig=File.open("scanResult_high_significance.dat", "w")
+
+
+  end
+end
+run_app()
 
 fitsList=File.open(fitsListAddress, "r")
 fitsFile=fitsList.readlines
@@ -53,16 +60,27 @@ fitsFile.each_with_index do |fitsName, index|
   scaleFactor=1.0/binWidth
   hist.Scale(scaleFactor)
 
+  maxSignificance=0
   for i in 1..binNum
     binCount=hist.GetBinContent(i)
     significance=(binCount-bgdMean)/bgdSigma
-    if significance>3.0 then
-      puts "#{fitsName} has #{significance.to_s} sigma bin."
+    if significance>maxSignificance then
+      maxSignificance=significance
     end
+    #if significance>3.0 then
+    #  puts "#{fitsName} has #{significance.to_s} sigma bin."
+    #end
   end
   message="#{(index+1).to_s}/#{(fitsFileNum+1).to_s} completed"
   puts message
+  string=fitsName+"\t"+maxSignificance.to_s
+  outputAll.puts(string)
+  if maxSignificance>3.0 then
+    outputSig.puts(string)
+  end    
 end
+outputAll.close()
+outputSig.close()
 
 =begin
 c0=Root::TCanvas.create("c0", "canvas0", 640, 480)
